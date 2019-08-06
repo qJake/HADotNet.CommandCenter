96d2f787e30497317c3c32e3caef03b7049942a8
qJake <jake@burgy.me>
2019-08-05 23:30:22 -0500

    Add basic admin area / nav

---
+++ HADotNet.CommandCenter/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HADotNet.CommandCenter.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Tiles()
+        {
+            return View();
+        }
+        public IActionResult Layout()
+        {
+            return View();
+        }
+        public IActionResult Settings()
+        {
+            return View();
+        }
+    }
+}
--- HADotNet.CommandCenter/wwwroot/js/app.ts
+++ HADotNet.CommandCenter/wwwroot/js/app.ts
@@ -2,7 +2,13 @@
 {
     constructor()
     {
-        
+        $(() => this.init());
+    }
+
+    private init(): void
+    {
+        $('.ui.accordion').accordion();
+        $('.ui.sidebar').sidebar();
     }
 } 
 var __app = new CommandCenter();
