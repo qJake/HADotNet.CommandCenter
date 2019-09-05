0b2619e17fd6dae63fab5cfd3434fb3ac8e27905
qJake <jake@burgy.me>
2019-09-04 20:57:55 -0500

    Add antiforgery

--- HADotNet.CommandCenter/Startup.cs
+++ HADotNet.CommandCenter/Startup.cs
@@ -63,6 +63,8 @@ namespace HADotNet.CommandCenter
                         .PersistKeysToFileSystem(new DirectoryInfo(@"/var/hacc-keys/"));
             }
 
+            services.AddAntiforgery();
+
             services.AddSingleton<IConfigStore, JsonConfigStore>();
             services.AddScoped(_ => ClientFactory.GetClient<EntityClient>());
             services.AddScoped(_ => ClientFactory.GetClient<StatesClient>());
