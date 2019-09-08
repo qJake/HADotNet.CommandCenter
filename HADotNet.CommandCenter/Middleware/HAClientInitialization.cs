3c1bf471102d631c52f6a402777c8cb614cba316
qJake <jake@burgy.me>
2019-09-08 10:19:33 -0500

    #27 Fix caching of Hassio Token

--- HADotNet.CommandCenter/Middleware/HAClientInitialization.cs
+++ HADotNet.CommandCenter/Middleware/HAClientInitialization.cs
@@ -26,21 +26,21 @@ namespace HADotNet.CommandCenter.Middleware
         {
             var config = await ConfigStore.GetConfigAsync();
 
-            if (!string.IsNullOrWhiteSpace(config?.Settings?.BaseUri) && !string.IsNullOrWhiteSpace(config?.Settings?.AccessToken))
+            if (!string.IsNullOrWhiteSpace(config?.Settings?.BaseUri) && (!string.IsNullOrWhiteSpace(config?.Settings?.AccessToken) || !string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("HASSIO_TOKEN"))))
             {
                 if (!ClientFactory.IsInitialized)
                 {
                     if (config.Settings.IsHassIo)
                     {
                         Log.LogInformation($"Auto-initializing HACC via Hass.io addon.");
+                        ClientFactory.Initialize(config.Settings.BaseUri, Environment.GetEnvironmentVariable("HASSIO_TOKEN"));
                     }
                     else
                     {
                         Log.LogInformation($"Initializing HACC API with URL {config?.Settings?.BaseUri ?? "[NULL]"} and access token [{new string(config?.Settings?.AccessToken.Take(6).ToArray())}•••••••••••{new string(config?.Settings?.AccessToken.TakeLast(6).ToArray())}].");
+                        ClientFactory.Initialize(config.Settings.BaseUri, config.Settings.AccessToken);
                     }
                 }
-
-                ClientFactory.Initialize(config.Settings.BaseUri, config.Settings.AccessToken);
             }
             else
             {
@@ -49,16 +49,15 @@ namespace HADotNet.CommandCenter.Middleware
 
             if (!ClientFactory.IsInitialized)
             {
-                var hassToken = Environment.GetEnvironmentVariable("HASSIO_TOKEN");
-
-                if (!string.IsNullOrWhiteSpace(hassToken))
+                // If we're in Hass.io mode, set the base URI and redirect to the admin homepage.
+                if (!string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("HASSIO_TOKEN")))
                 {
                     await ConfigStore.ManipulateConfig(c =>
                     {
                         c.Settings = new SystemSettings
                         {
                             BaseUri = "http://hassio/homeassistant",
-                            AccessToken = hassToken,
+                            AccessToken = null,
                             IsHassIo = true
                         };
 
@@ -67,6 +66,7 @@ namespace HADotNet.CommandCenter.Middleware
                     });
                 }
 
+                // Otherwise, if we aren't on one of the approved pages, redirect to the settings page and prompt for setup.
                 if (context.Request.Path.ToString().ToLower() != "/admin/settings" && context.Request.Path.ToString().ToLower() != "/admin")
                 {
                     Log.LogInformation($"Client factory is not initialized, redirecting user to settings area...");
