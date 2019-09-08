9989f706784b910fc1192bfbdf641debf3bdd4e4
qJake <jake@burgy.me>
2019-09-07 22:35:44 -0500

    Rename arm to arm64, remove Windows for now

---
+++ Docker/BuildHaccContainers.ps1
@@ -0,0 +1,10 @@
+﻿$version = '0.5.0'
+
+cd W:\GitHub\HADotNet.CommandCenter\Docker\
+
+docker build --no-cache -t qjake/hacc:$version-linux-alpine-amd64 -f linux-alpine-amd64/Dockerfile .
+docker build --no-cache -t qjake/hacc:$version-linux-debian-amd64 -f linux-debian-amd64/Dockerfile .
+docker build --no-cache -t qjake/hacc:$version-linux-ubuntu-amd64 -f linux-ubuntu-amd64/Dockerfile .
+docker build --no-cache -t qjake/hacc:$version-linux-alpine-arm64v8 -f linux-alpine-arm64v8/Dockerfile .
+
+docker tag qjake/hacc:$version-linux-debian-amd64 qjake/hacc:latest
