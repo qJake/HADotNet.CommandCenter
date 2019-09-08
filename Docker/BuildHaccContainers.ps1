80f933df256bdc1201be94789970b03c5552185e
qJake <jake@burgy.me>
2019-09-08 01:32:52 -0500

    More Pwsh updates

--- Docker/BuildHaccContainers.ps1
+++ Docker/BuildHaccContainers.ps1
@@ -30,6 +30,15 @@ docker build --build-arg TAG=$version --no-cache -t qjake/hacc:$version-linux-de
 
 Write-Progress -Activity $prgAct -Completed
 
+
+# Tag Latest Specific
+
+docker tag qjake/hacc:$version-linux-debian-arm64v8 qjake/hacc:latest-linux-debian-arm64v8
+docker tag qjake/hacc:$version-linux-ubuntu-amd64 qjake/hacc:latest-linux-ubuntu-amd64
+docker tag qjake/hacc:$version-linux-alpine-amd64 qjake/hacc:latest-linux-alpine-amd64
+docker tag qjake/hacc:$version-linux-debian-arm32v7 qjake/hacc:latest-linux-debian-arm32v7
+docker tag qjake/hacc:$version-linux-debian-amd64 qjake/hacc:latest-linux-debian-amd64
+
 # Re-tag Hassio Architecture Tags
 
 docker tag qjake/hacc:$version-linux-debian-arm64v8 qjake/hacc-hassio-aarch64:$version
@@ -49,13 +58,35 @@ docker tag qjake/hacc-hassio-armv7:$version   qjake/hacc-hassio-armv7:latest
 docker tag qjake/hacc:$version-linux-debian-amd64 qjake/hacc:$version
 docker tag qjake/hacc:$version qjake/hacc:latest
 
+
 # Upload them all in order
 
+$prgAct = 'Pushing Docker images...'
+$prgStat = 'Pushing'
+
+# The first 5 take awhile, the others are just copies and go faster.
+Write-Progress -Activity $prgAct -Status $prgStat -PercentComplete 15 -CurrentOperation 'Alpine-AMD64'
 docker push qjake/hacc:$version-linux-alpine-amd64
+
+Write-Progress -Activity $prgAct -Status $prgStat -PercentComplete 30 -CurrentOperation 'Debian-AMD64'
 docker push qjake/hacc:$version-linux-debian-amd64
+
+Write-Progress -Activity $prgAct -Status $prgStat -PercentComplete 50 -CurrentOperation 'Ubuntu-AMD64'
 docker push qjake/hacc:$version-linux-ubuntu-amd64
+
+Write-Progress -Activity $prgAct -Status $prgStat -PercentComplete 75 -CurrentOperation 'Debian-ARM32v7'
 docker push qjake/hacc:$version-linux-debian-arm32v7
+
+Write-Progress -Activity $prgAct -Status $prgStat -PercentComplete 90 -CurrentOperation 'Debian-ARM64v8'
 docker push qjake/hacc:$version-linux-debian-arm64v8
+
+Write-Progress -Activity $prgAct -Completed
+
+docker push qjake/hacc:latest-linux-alpine-amd64
+docker push qjake/hacc:latest-linux-debian-amd64
+docker push qjake/hacc:latest-linux-ubuntu-amd64
+docker push qjake/hacc:latest-linux-debian-arm32v7
+docker push qjake/hacc:latest-linux-debian-arm64v8
 docker push qjake/hacc-hassio-aarch64:$version
 docker push qjake/hacc-hassio-amd64:$version
 docker push qjake/hacc-hassio-armhf:$version
