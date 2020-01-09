f81e966fc7a7275928875509d6b8168a6943f948
qJake <jake@burgy.me>
2020-01-08 20:09:01 -0600

    Fix person tiles not respecting base url

--- HADotNet.CommandCenter/wwwroot/js/tiles/person.tile.ts
+++ HADotNet.CommandCenter/wwwroot/js/tiles/person.tile.ts
@@ -13,7 +13,7 @@ class PersonTile extends Tile
     public updateState(state: IHAStateChangedData): void
     {
         //console.log("State received for: " + tile.name, state);
-        let picture = state.new_state.attributes['entity_picture'] ? state.new_state.attributes['entity_picture'].toString() : '';
+        let picture: string = state.new_state.attributes['entity_picture'] ? state.new_state.attributes['entity_picture'].toString() : '';
         let location = state.new_state.state.replace('_', ' ');
         let label = state.new_state.attributes['friendly_name'].toString();
         if (this.tile.overrideLabel)
@@ -23,9 +23,16 @@ class PersonTile extends Tile
 
         let isHome = location.toLowerCase() === 'home';
 
+        // Adjust base URL
+        if (!picture.toLowerCase().startsWith('http'))
+        {
+            picture = window.ccOptions.baseUrl + picture;
+        }
+
         $(`#tile-${this.tile.name}`).find('span[value-name]').text(label);
         $(`#tile-${this.tile.name}`).find('span[value-location]').text(location);
         $(`#tile-${this.tile.name}`).find('span[value-picture]').css('background-image', `url(${picture})`).removeClass('bw');
+
         if (!isHome)
         {
             $(`#tile-${this.tile.name}`).find('span[value-picture]').addClass('bw');
